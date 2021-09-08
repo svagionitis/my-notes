319202806c2539b60facb1aef9453cdc0c3aa5b7
Stavros Vagionitis <svag@miltech.gr>
2021-09-08 09:53:37 +0300

    useful_scripts: add git-fetch-all-dir-recursively.sh
    A script to git fetch repos in directories recursively

---
+++ useful_scripts/git-fetch-all-dir-recursively.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -eu
+# Git fetch git repos in directories recursively
+
+usage()
+{
+    echo "Usage: $0 <directory>"
+}
+
+if [ -z "${1}" ]; then
+    usage
+    exit 1
+fi
+INPUT_DIR=${1}
+
+# See https://stackoverflow.com/questions/11981716/how-to-quickly-find-all-git-repos-under-a-directory
+GIT_REPOS_DIR=$(find "${INPUT_DIR}" -type d -execdir test -d {}/.git \; -prune -print)
+
+for dir in ${GIT_REPOS_DIR}; do
+    cd "${dir}"
+    echo "Updating git repo in ${dir}"
+    git fetch --all
+    cd -
+done
